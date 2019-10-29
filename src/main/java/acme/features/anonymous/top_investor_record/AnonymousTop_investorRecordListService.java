2a8cd858794927ca7f59ea84efde8fdf88bb721f
juann <juann@DESKTOP-VIODC0Q>
2019-10-29 19:45:17 +0100

    Top investor records finished

---
+++ src/main/java/acme/features/anonymous/top_investor_record/AnonymousTop_investorRecordListService.java
@@ -0,0 +1,49 @@
+
+package acme.features.anonymous.top_investor_record;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.investor_records.Investor_record;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.entities.Anonymous;
+import acme.framework.services.AbstractListService;
+
+@Service
+public class AnonymousTop_investorRecordListService implements AbstractListService<Anonymous, Investor_record> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	AnonymousTop_investor_recordRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Investor_record> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public void unbind(final Request<Investor_record> request, final Investor_record entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "name", "sector", "investing_statement", "stars");
+	}
+
+	@Override
+	public Collection<Investor_record> findMany(final Request<Investor_record> request) {
+		assert request != null;
+
+		Collection<Investor_record> result;
+
+		result = this.repository.findManyAll();
+		return result;
+	}
+}
