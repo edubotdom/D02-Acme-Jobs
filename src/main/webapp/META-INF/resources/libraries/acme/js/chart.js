391792bda78fc68f54bb3467a67b647dece4f05d
MJ <MJ@Alien.Home>
2019-11-02 23:37:17 +0100

    Chart css and js finished

--- src/main/webapp/META-INF/resources/libraries/acme/js/chart.js
+++ src/main/webapp/META-INF/resources/libraries/acme/js/chart.js
@@ -10,33 +10,34 @@ function grafica(ctx, tipo, datos, etiquetas ,titulo){
 			scales: {
         yAxes: [{
           gridLines: {
-            color: "rgba(255, 255, 255, 0.2)"
+            color: "rgba(0, 0, 0, 0.15)",
           },
 	        ticks: {
-            suggestedMin: 0,//este es importante
+            suggestedMin: 0,
             /*suggestedMax: 10,*/
 						stepSize: 1,
 	        }
         }],
         xAxes: [{
           gridLines: {
-            color: "rgba(0, 2, 2, 0.2)"
+            color: "rgba(0, 0, 0, 0.15)"
           }
         }]
+      },
+      tooltips:{
+        enabled:false
       },
 			title:{
 				display:true,
 				text:titulo,
-				fontSize: 18
+				fontSize: 20
 			},
 			elements: {
           line: {
-            tension: 0.3 // disables bezier curves
+            tension: 0 // disables bezier curves
           }
         },
-			tooltips:{
-				display:false
-			},
+
 			legend:{
 				display:false
 			},
@@ -45,6 +46,8 @@ function grafica(ctx, tipo, datos, etiquetas ,titulo){
 
 	Chart.defaults.global.defaultFontColor = '#3a3737';
 	Chart.defaults.global.defaultFontFamily = 'Lato';
+  //Chart.defaults.global.defaultFontSize = 12;
+
 
 	var c = new Chart(ctx, {
 		type: tipo,
@@ -61,22 +64,62 @@ function datosGrafica(datos, etiquetas){
 		datasets: [{
 			data: datos,
 			backgroundColor: [
-				'rgba(255, 99, 132, 0.45)',
-				'rgba(54, 162, 235, 0.45)',
-				'rgba(255, 206, 86, 0.45)',
-				'rgba(75, 192, 192, 0.45)',
-				'rgba(153, 102, 255, 0.45)',
-				'rgba(255, 159, 64, 0.45)',
-				'rgba(255, 99, 132, 0.45)',
-				'rgba(54, 162, 235, 0.45)',
-				'rgba(255, 206, 86, 0.45)',
-				'rgba(75, 192, 192, 0.45)',
-				'rgba(153, 102, 255, 0.45)',
-				'rgba(255, 159, 64, 0.45)'
+				'rgba(185, 84, 106, 0.35)',
+				'rgba(59, 153, 216, 0.35)',
+				'rgba(255, 206, 86, 0.35)',
+				'rgba(67, 228, 75, 0.35)',
+				'rgba(245, 112, 183, 0.35)',
+				'rgba(64, 255, 83, 0.35)',
+				'rgba(176, 232, 32, 0.35)',
+				'rgba(76, 107, 181, 0.35)',
+				'rgba(68, 224, 122, 0.35)',
+				'rgba(210, 62, 62, 0.35)',
+				'rgba(222, 89, 236, 0.35)',
+				'rgba(179, 23, 23, 0.35)'
 				],
-			borderColor: "#827d7d",
+      hoverBackgroundColor: [
+				'rgba(185, 84, 106, 0.55)',
+				'rgba(59, 153, 216, 0.55)',
+				'rgba(255, 206, 86, 0.55)',
+				'rgba(67, 228, 75, 0.55)',
+				'rgba(245, 112, 183, 0.55)',
+				'rgba(64, 255, 83, 0.55)',
+				'rgba(176, 232, 32, 0.55)',
+				'rgba(76, 107, 181, 0.55)',
+				'rgba(68, 224, 122, 0.55)',
+				'rgba(210, 62, 62, 0.55)',
+				'rgba(222, 89, 236, 0.55)',
+				'rgba(179, 23, 23, 0.55)'
+				],
+			borderColor:  [
+        'rgba(185, 84, 106, 0.75)',
+        'rgba(59, 153, 216, 0.75)',
+        'rgba(255, 206, 86, 0.75)',
+        'rgba(67, 228, 75, 0.75)',
+        'rgba(245, 112, 183, 0.75)',
+        'rgba(64, 255, 83, 0.75)',
+        'rgba(176, 232, 32, 0.75)',
+        'rgba(76, 107, 181, 0.75)',
+        'rgba(68, 224, 122, 0.75)',
+        'rgba(210, 62, 62, 0.75)',
+        'rgba(222, 89, 236, 0.75)',
+        'rgba(179, 23, 23, 0.75)'
+				],
 			borderWidth: 1,
-			hoverBorderColor:"#383737"
+			hoverBorderColor: [
+        'rgba(185, 84, 106, 1)',
+        'rgba(59, 153, 216, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(67, 228, 75, 1)',
+        'rgba(245, 112, 183, 1)',
+        'rgba(64, 255, 83, 1)',
+        'rgba(176, 232, 32, 1)',
+        'rgba(76, 107, 181, 1)',
+        'rgba(68, 224, 122, 1)',
+        'rgba(210, 62, 62, 1)',
+        'rgba(222, 89, 236, 1)',
+        'rgba(179, 23, 23, 1)'
+				]
 		}]
 	}
 }
